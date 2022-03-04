5558a273a435ff2aedf5191063996703f2703e52
demonlord27 <73150420+demonlord27@users.noreply.github.com>
2022-03-04 10:58:36 +0530

    Update project3.js

--- mod4/project3.js
+++ mod4/project3.js
@@ -1,11 +1,11 @@
-var names = ["John", "Sam", "James", "George", "Jack", "Robert", "Clint", "Joe", "Leo", "Ron"];
+var names = ["Yaakov", "John", "Jen", "jason", "paul", "frank", "larry", "paula", "laura", "jim"];
 console.log("Array of Names: " + names);
 
 for (var i in names){
 	if (names[i][0]==="j" || names[i][0]==="J"){
-		console.log("Hello " + names[i])
+		console.log("Good Bye " + names[i])
 	}
 	else{
-		console.log("Goodbye " + names[i])
+		console.log("Hello " + names[i])
 	}
 };
